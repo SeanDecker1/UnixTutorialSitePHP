f73489718d1e361e0d555b793acd267c11fe332e
SeanDecker1 <43579108+SeanDecker1@users.noreply.github.com>
2019-03-26 21:59:15 -0400

    Uploaded Project to New Repo

---
+++ about/index.php
@@ -0,0 +1,63 @@
+<?php
+	$title = 'About Us';
+    $active_about = 'active';
+    $path='../';
+	$head_title =   '
+				    <h2 class="head-title">About Us</h2>
+                    ';
+	include($path.'assets/inc/header.php');
+?>
+
+        <div class="tut-light">
+            
+            <div class="tut-option">
+                <h3>Andrew Bentkowski</h3>
+                <p>Major: CIT</p><br/>
+                <p>Role: Andrew was the team leader of the group and offered large contributions in the content creation for the website, organizing terms, taking all screenshots for the tutorial pages and created content pages. As the team leader, Andrew picked up in many areas that were in need of assistance and held meetings so the group could be on the same page at all times.</p>
+            </div>   
+            
+            <div class="tut-dash"></div>
+            
+            <div class="tut-option">
+                <h3>Dominico Tran</h3>
+                <p>Major: WMC</p><br/>
+                <p>Role: His role consists of providing any type of content whether it was description of a command, history of linux/unix, and describing what the goals of the website is aiming to do for anyone interested in learning unix. Some of his key contributions were content of the homepage, history of unix/linux,  and the descriptions/explanations of some of the commands.</p>
+            </div>
+            
+            <div class="tut-dash"></div>
+            
+            <div class="tut-option">
+                <h3>Efmajackson Rosario</h3>
+                <p>Major: CIT</p><br/>
+                <p>Role: Some of his key project contributions to this project, were the initial vision of the site, debugging errors on site, mobile optimization, quiz forms, and presentable content.</p>
+            </div>
+            
+            <div class="tut-dash"></div>
+            
+            <div class="tut-option">
+                <h3>Perry Wu</h3>
+                <p>Major: HCC</p><br/>
+                <p>Role: His team roles are primary graphic designer and secondary information architect. Some key project contributions include designing the team logo, creating initial quiz forms, and organizing tutorial content in an intuitive manner.</p>
+            </div>
+            
+            <div class="tut-dash"></div>
+            
+            <div class="tut-option">
+                <h3>Rahul Shah</h3>
+                <p>Major: HCC</p><br/>
+                <p>Role: His team roles include Interaction Designer and Information Architect. Some key project contributions include: heavy involvement with the initial design, goals, and content structure as well as fine tuning the look and feel of the site to align with its goals. Also created early versions of the quiz content and handled the organization of tutorials and command listing pages.</p>
+            </div>
+            
+            <div class="tut-dash"></div>
+            
+            <div class="tut-option">
+                <h3>Sean Decker</h3>
+                <p>Major: CIT</p><br/>
+                <p>Role: His team role was primary Information Architect. He played a part in nearly every single piece of the website creation. This includes creating the initial and final design, developing the php includes for each page, optimizing the entire site for mobile, debugging, formatting code, working in JavaScript functionality, developing the navigation system, and deciding where the information was going.</p>
+            </div>
+        
+        </div>
+
+<?php
+	include ($path.'assets/inc/footer.php');
+?>
